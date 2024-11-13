6b997e71af437487a89d2c324e277afc22372df1
Mariana Ponce <atilanoponcemariana18@gmail.com>
2024-11-12 18:45:45 -0600

    EXAMEN PRÁCTICO PIZZERIA 12-11-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import CapturaPedidoComponent from "./formularios/captura-pedido/pedido.component";
+import DetallePedidoComponent from "./formularios/detalle-pedido/detalle-pedido.component";
+import VentasDiaComponent from "./formularios/ventas-dia/ventas-dia.component";
+/*import FormularioClienteComponent from "./formularios/formulario-cliente/formulario-cliente.component";
+import { FormularioPizzaComponent } from "./formularios/formulario-pizza/formulario-pizza.component";
+import { DetallePedidoComponent } from "./formularios/detalle-pedido/detalle-pedido.component";*/
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CapturaPedidoComponent, DetallePedidoComponent, VentasDiaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+    // otras rutas necesarias
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
